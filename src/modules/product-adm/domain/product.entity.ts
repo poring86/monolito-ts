d330b16e109fdb7673f4cb6908fc7b097bba7e8b
Matheus Lino <matheuslino86@gmail.com.br>
2023-04-20 18:21:59 -0400

    feat: :sparkles: First commit

---
+++ src/modules/product-adm/domain/product.entity.ts
@@ -0,0 +1,48 @@
+import Id from "../../../modules/shared/domain/value-object/id.value-object";
+import AggregateRoot from "../../shared/domain/entity/aggregate-root.interface";
+import BaseEntity from "../../../modules/shared/domain/entity/base.entity";
+
+type ProductProps = {
+  id?: Id;
+  name: string;
+  description: string;
+  purchasePrice: number;
+  stock: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export default class Product extends BaseEntity implements AggregateRoot {
+  private _name: string;
+  private _description: string;
+  private _purchasePrice: number;
+  private _stock: number;
+
+  constructor(props: ProductProps) {
+    super(props.id);
+    this._name = props.name;
+    this._description = props.description;
+    this._purchasePrice = props.purchasePrice;
+    this._stock = props.stock;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  get purchasePrice(): number {
+    return this._purchasePrice;
+  }
+
+  get stock(): number {
+    return this._stock;
+  }
+
+  set stock(stock: number) {
+    this._stock = stock;
+  }
+}
---
+++ src/modules/product-adm/usecase/add-product/add-product.dto.ts
@@ -0,0 +1,18 @@
+import { string } from "yup";
+export interface AddProductInputDto {
+  id?: string;
+  name: string;
+  description: string;
+  purchasePrice: number;
+  stock: number;
+}
+
+export interface AddProductOutputDto {
+  id: string;
+  name: string;
+  description: string;
+  purchasePrice: number;
+  stock: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
